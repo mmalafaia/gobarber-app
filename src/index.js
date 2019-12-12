e24d49fdcdf63a62eeff07305ff2140c7e3076f7
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-12 17:01:44 -0300

    Commit: New Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Routes from './routes';
+
 export default function App() {
   return (
-    <View>
+    <Routes>
       <Text>Hello World</Text>
-    </View>
+    </Routes>
   );
 }
